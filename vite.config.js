ba74fb3ba674f736f34040e40847149e76912b42
Flere21 <aaronhans2134@gmail.com>
2026-06-22 23:50:37 +0800

    add vue setup and github actions

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  // REPLACE this with your actual GitHub repository name!
+  base: '/my-portfolio/', 
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
